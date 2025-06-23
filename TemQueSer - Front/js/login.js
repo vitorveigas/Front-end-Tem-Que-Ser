17c00d6625646ae250fd3971ab91c8cb150ca647
Vitor Veiga <vitorveiga0621@gmail.com>
2025-06-23 19:20:54 -0300

    Update login.js

--- TemQueSer - Front/js/login.js
+++ TemQueSer - Front/js/login.js
@@ -23,7 +23,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
             emailSocio: email,
             senhaSocio: senha
         };
-        endpoint = 'https://temqueserapi.onrender.com/auth/login-socio';
+        endpoint = 'https://temqueserapi.fly.dev//auth/login-socio';
         userType = 'sócio';
     } else {
         loginData = {
